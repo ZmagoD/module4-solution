ef45f2078f1486ccb1b7b856d6af1f3d7e6af55f
ZmagoD <zmago_devetak@yahoo.com>
2016-10-12 23:07:51 +0200

    completed module4

---
+++ src/routes.js
@@ -0,0 +1,40 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('MenuApp')
+    .config(RoutesConfig);
+
+  RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', { url: '/',
+                       templateUrl: 'src/menuApp/home.html' })
+      .state('categories', {
+        url: '/categories',
+        templateUrl: 'src/categories/categories.html',
+        controller: 'CategoriesController as categoryList',
+        resolve: {
+          categories: ['MenuDataService', function(MenuDataService) {
+            return MenuDataService.getAllCategories();
+          }]
+        }
+      })
+      .state('items', {
+        url: '/items/{categoryId}',
+        templateUrl: 'src/items/items.html',
+        controller: 'ItemsController as itemList',
+        params: {
+          categoryId: null
+        },
+        resolve: {
+          items: ['$stateParams', 'MenuDataService', function($stateParams, MenuDataService) {
+            return MenuDataService.getItemsForCategory($stateParams.categoryId);
+          }]
+        }
+      });
+
+    $urlRouterProvider.otherwise('/');
+  }
+})();
